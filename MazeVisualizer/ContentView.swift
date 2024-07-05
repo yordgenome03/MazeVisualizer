1012cef38932890e3946a306c7e32df10d0c4d32
YomEngine <120723438+yordgenome03@users.noreply.github.com>
2024-07-06 05:40:18 +0900

    Initial Commit

---
+++ MazeVisualizer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MazeVisualizer
+//
+//  Created by yotahara on 2024/07/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MazeVisualizer/MazeVisualizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MazeVisualizerApp.swift
+//  MazeVisualizer
+//
+//  Created by yotahara on 2024/07/06.
+//
+
+import SwiftUI
+
+@main
+struct MazeVisualizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
